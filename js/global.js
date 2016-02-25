55409749a2067938bd646568909981f9fcf7d0ed
Akash Gaonkar <akashgaonkar@gmail.com>
2016-02-25 15:58:09 -0700

    Setup generated page content and sidebar.

--- js/global.js
+++ js/global.js
@@ -1,28 +1,82 @@
 
-// if (typeof(DOTSGlobal) === 'undefined') DOTSGlobal = {};
+if (typeof(DOTSGlobal) === 'undefined') DOTSGlobal = {};
 
-// DOTSGlobal.to_run_on_load = [];
+DOTSGlobal.XMLGetRequest = function(url, onSuccess, params) {
+	
+	if (params && params.length > 0) url += "?" + DOTSGlobal.urlEncode(params);
 
-// DOTSGlobal.runOnLoad = function(f){
-//     DOTSGlobal.to_run_on_load.push(f);
-// }
+	request = DOTSGlobal.__createXMLRequest("GET", url, onSuccess);
 
-// window.addEventListener('WebComponentsReady', function(){
-//     for (var i = 0; i != DOTSGlobal.to_run_on_load.length; ++i) {
-//         DOTSGlobal.to_run_on_load[i]();
-//     }
-// });
+	request.send();
+
+}
+
+DOTSGlobal.XMLPostRequest = function(url, onSuccess, params) {
+	
+	request = DOTSGlobal.__createXMLRequest("POST", url, onSuccess);
+
+	request.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+
+	if (params && params.length > 0) request.send(DOTSGlobal.urlEncyouode(params));
+	else request.send();
+
+}
+
+DOTSGlobal.urlEncode = function(params) {
+	
+	var encoded = "";
+	var first = true;
+
+	for (var key in params) {
+
+		if (Array.isArray(params[key])) {
+
+			var items = params[key];
+			var encodedKey = encodeURIComponent(key) + "[]";
+			
+			for (var i = 0; i < items.length; i++) {
+				
+				if (first) first = false;
+				else encoded += "&";
+
+				encoded += encodedKey + "=" + encodeURIComponent(items[i]);
+			}
+
+		} else {
+
+			if (first) first = false;
+			else encoded += "&";
+
+			encoded += encodeURIComponent(key) + "=" + encodeURIComponent(params[key]);
+
+		}
+
+	}
+
+	return encoded;
 
 
-// DOTSGlobal.bringBackTransitions = function() {
-//     no_transitions_class = 'no-transitions-while-pageload';
+}
+DOTSGlobal.__createXMLRequest = function(type, url, onSuccess) {
+	
+	var request = new XMLHttpRequest();
+	
+	request.open(type, url, true);
 
-//     var trn_less = document.querySelectorAll("."+no_transitions_class);
+	request.onload = function() {
+	  	if (request.status >= 200 && request.status < 400) {
+		    // Success!
+		    onSuccess(request.responseText);
+		} else {
+		    // We reached our target server, but it returned an error
+		    throw "XML HTTP request failed: "+request.status+" @ "+url;
+		}
+	};
 
-//     for (var i = 0; i != trn_less.length; ++i) {
-//         trn_less[i].classList.remove(no_transitions_class);
-//     }
-// }
+	request.onerror = function() {
+		// There was a connection error of some sort
+		throw "XML HTTP request failed: Could not connect to "+url;
+	};
 
-
-// DOTSGlobal.runOnLoad(DOTSGlobal.bringBackTransitions);
+	return request;
+}
