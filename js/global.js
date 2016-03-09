cfa12fe4df74657285fe0c080750a4bae910a023
Akash Gaonkar <akashgaonkar@gmail.com>
2016-03-09 16:13:43 -0700

    Tried to fix github page.

--- js/global.js
+++ js/global.js
@@ -1,7 +1,7 @@
 
 if (typeof(DOTSGlobal) === 'undefined') DOTSGlobal = {};
 
-DOTSGlobal.rootURL = "./";
+DOTSGlobal.rootURL = "/DesignOfThisSite/";
 
 DOTSGlobal.XMLGetRequest = function(url, onSuccess, params) {
 	
